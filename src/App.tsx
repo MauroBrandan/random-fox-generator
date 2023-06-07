24f712dfd98093a8ceda51075eecc90f18bd8bbf
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-06-07 13:59:25 -0300

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<main className='w-full grid place-items-center'>
+			<h1 className='text-5xl font-bold '>Random Fox Generator</h1>
+		</main>
+	)
+}
+
+export default App
