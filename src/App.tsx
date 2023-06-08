5105ef7cb0ac1b77c1120be687eae2413876766f
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-06-08 12:50:05 -0300

    Type Fox for useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
+import { useState } from 'react'
 import RandomFox from './components/RandomFox'
 
+type Fox = {
+	src: string
+	alt: string
+}
+
 function App() {
+	const [foxes, setFoxes] = useState<Fox[]>([
+		{ src: 'https://randomfox.ca/images/5.jpg', alt: 'Random fox number 5' },
+		{ src: 'https://randomfox.ca/images/13.jpg', alt: 'Random fox number 13' },
+		{ src: 'https://randomfox.ca/images/25.jpg', alt: 'Random fox number 25' }
+	])
+
 	return (
 		<main className='w-full grid place-items-center'>
-			<h1 className='text-5xl font-bold '>Random Fox Generator</h1>
-			<section>
-				<RandomFox
-					src='https://randomfox.ca/images/5.jpg'
-					alt='Random fox number 5'
-				/>
+			<h1 className='text-2xl font-bold md:text-5xl'>Random Fox Generator</h1>
+			<section className='w-full h-auto grid grid-cols-fox place-items-center gap-5'>
+				{foxes.map((fox) => (
+					<RandomFox src={fox.src} alt={fox.alt} />
+				))}
 			</section>
 		</main>
 	)
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        // Fox grid
+        'fox': 'repeat(auto-fit, minmax(320px, 1fr))',
+      }
+    },
   },
   plugins: [],
 }
