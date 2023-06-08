69129f719c0ad8249a4258b0f5e4eec527837d7f
Mauro Brandan <maurobrandan.mb@gmail.com>
2023-06-08 13:23:55 -0300

    Generate random foxes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { random } from 'lodash'
+import type { MouseEventHandler } from 'react'
 import RandomFox from './components/RandomFox'
 
 type Fox = {
@@ -7,18 +9,30 @@ type Fox = {
 }
 
 function App() {
-	const [foxes, setFoxes] = useState<Fox[]>([
-		{ src: 'https://randomfox.ca/images/5.jpg', alt: 'Random fox number 5' },
-		{ src: 'https://randomfox.ca/images/13.jpg', alt: 'Random fox number 13' },
-		{ src: 'https://randomfox.ca/images/25.jpg', alt: 'Random fox number 25' }
-	])
+	const [foxes, setFoxes] = useState<Fox[]>([])
+
+	const addNewFox: MouseEventHandler<HTMLButtonElement> = () => {
+		const id = random(1, 120)
+
+		const newFox: Fox = {
+			src: `https://randomfox.ca/images/${id}.jpg`,
+			alt: `Random fox number ${id}`
+		}
+
+		setFoxes((prevState) => [...prevState, newFox])
+	}
 
 	return (
-		<main className='w-full grid place-items-center'>
+		<main className='w-full grid place-items-center gap-10'>
 			<h1 className='text-2xl font-bold md:text-5xl'>Random Fox Generator</h1>
+
+			<button onClick={addNewFox} className='bg-blue-700 rounded-3xl p-2'>
+				Generate a new Fox
+			</button>
+
 			<section className='w-full h-auto grid grid-cols-fox place-items-center gap-5'>
-				{foxes.map((fox) => (
-					<RandomFox src={fox.src} alt={fox.alt} />
+				{foxes.map((fox, index) => (
+					<RandomFox key={index} src={fox.src} alt={fox.alt} />
 				))}
 			</section>
 		</main>
